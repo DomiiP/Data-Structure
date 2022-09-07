bbd6d24f6c3f61cba8cbbaf7923c8ed3dc532093
DomiiP <95188800+DomiiP@users.noreply.github.com>
2022-09-07 23:29:21 +0200

    Update 1.cpp
    Cleaner! :)

--- CharacterArrayString/1.cpp
+++ CharacterArrayString/1.cpp
@@ -6,24 +6,13 @@ int main()
     // cin.getline(a,1000,'#');
     char a;
     a = cin.get();
-    int alpha = 0;
-    int digit = 0;
-    int space = 0;
+    int alpha = 0, digit = 0, space = 0;
 
     while (a != '\n')
     {
-        if (a >= '0' and a <= '9')
-        {
-            digit++;
-        }
-        else if (a >= 'a' and a <= 'z' or a >= 'A' and a <= 'Z')
-        {
-            alpha++;
-        }
-        else if (a == ' ' )
-        {
-            space++;
-        }
+        if (a >= '0' and a <= '9')digit++;
+        else if (a >= 'a' and a <= 'z' or a >= 'A' and a <= 'Z') alpha++;
+        else if (a == ' ' )space++;
         a=cin.get();
     }
     cout<<"digit"<<digit<<endl;
